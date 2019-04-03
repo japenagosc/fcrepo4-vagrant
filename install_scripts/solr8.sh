5f54b943ebb01dae802157a7fb8b58b2c83e2bb9
Jaime Penagos <jaime.penagos@lmu.de>
2019-04-03 13:28:15 +0200

    2 instances of solr added

--- install_scripts/solr8.sh
+++ install_scripts/solr8.sh
@@ -7,6 +7,10 @@ if  ! -d /home/vagrant/solr-8.0.0 ; then
 
 	cd /home/vagrant/
 	
+	#echo -n "Downloading Fedora 4... $RELEASES/$WEBAPP"
+	#curl -o http://apache.lauf-forum.at/lucene/solr/8.0.0/solr-8.0.0.tgz
+	#echo " done"
+	
 	wget http://apache.lauf-forum.at/lucene/solr/8.0.0/solr-8.0.0.tgz
 	sleep 3
 	
@@ -16,10 +20,11 @@ if  ! -d /home/vagrant/solr-8.0.0 ; then
 	/home/vagrant/solr-8.0.0/bin/install_solr_service.sh /home/vagrant/solr-8.0.0.tgz
 	sleep 3
 	
-	su solr -c "solr start"
-	su solr -c "/opt/solr/bin/solr create -c kimws19"
+	#su solr -c "solr start"
+	su solr -c "/opt/solr/bin/solr create -c va-meta"
+	su solr -c "/opt/solr/bin/solr create -c va-daten"
 	sleep 3
 	
-	su vagrant -c "wget -O /usr/local/bin/fcr-listen https://github.com/birkland/fcr-listen/releases/download/0.0.1/fcr-listen-`uname -s`-`uname -m`"
-	su vagrant -c "chmod +x /usr/local/bin/fcr-listen"
+	wget -O /usr/local/bin/fcr-listen https://github.com/birkland/fcr-listen/releases/download/0.0.1/fcr-listen-`uname -s`-`uname -m`
+	chmod +x /usr/local/bin/fcr-listen
 fi 
